e39f8d6cace600b1f6e300fd8e55433f01f9fb4d
JoshGrant5 <joshgg@icloud.com>
2020-09-24 00:51:58 +0000

    Pair Programmed by Josh Grant, Ayushi Sharma, & Nicholas Kostos

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,43 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'N', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'K', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'N', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'K', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CANDY')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if matrix is empty", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isUndefined(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
+/* Pair Programmed by Josh Grant, Ayushi Sharma, & Nicholas Kostos */
+
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return undefined
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalJoin = [];
+    for (let i = 0; i < letters.length; i++) {
+        let newWord = "";
+        for (let j = 0; j < letters[i].length; j++) {
+            newWord += letters[j][i];
+        }
+        verticalJoin.push(newWord);
+    }
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
